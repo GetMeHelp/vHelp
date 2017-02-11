bec2c36b03fd4cade0d09cf81323590b1e27a493
Himanshu Singh <himanshusingh2407@gmail.com>
2017-02-11 17:03:34 +0530

    Added dist content

--- app/scripts/authenticate.js
+++ app/scripts/authenticate.js
@@ -1,12 +1,12 @@
 function onSignIn(googleUser) {
   // Useful data for your client-side scripts:
   var profile = googleUser.getBasicProfile();
-  console.log("ID: " + profile.getId()); // Don't send this directly to your server!
+  console.log('ID: ' + profile.getId()); // Don't send this directly to your server!
   console.log('Full Name: ' + profile.getName());
   console.log('Given Name: ' + profile.getGivenName());
   console.log('Family Name: ' + profile.getFamilyName());
-  console.log("Image URL: " + profile.getImageUrl());
-  console.log("Email: " + profile.getEmail());
+  console.log('Image URL: ' + profile.getImageUrl());
+  console.log('Email: ' + profile.getEmail());
 
   let userDetails = {
     name: profile.getName(),
@@ -20,7 +20,7 @@ function onSignIn(googleUser) {
   setFirebaseData(profile.getId(), userDetails);
 
   userDetails.id = profile.getId()
-  localStorage.setItem("userDetails", JSON.stringify(userDetails));
+  localStorage.setItem('userDetails', JSON.stringify(userDetails));
 
   // window.location.href = './needHelp.html';
 };
